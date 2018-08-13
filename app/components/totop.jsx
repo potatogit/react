1beea873f21fad65ee06e4684f31b7dd6df2ec2a
yang_yu <yang_yu@worksap.co.jp>
2018-08-13 18:41:37 +0800

    add image.

--- app/components/totop.jsx
+++ app/components/totop.jsx
@@ -1,11 +1,12 @@
 require('./totop.css');
 import React from 'react';
+import topIcon from '../images/scroll-to-top-icon-new.png';
 
 let ToTop = React.createClass({
 	render: function() {
 		return (
 			<div className="bottom-to-top">
-				<img src=" ./images/scroll-to-top-icon-new.png"/>
+				<img src={topIcon} alt="" />
 			</div>
 		);
 	}
